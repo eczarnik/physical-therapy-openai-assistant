14f4ace42465a8cdb71f68146e9ccf5cac84b902
Elaine Czarnik <eczarnik@umich.edu>
2023-04-04 23:14:16 -0400

    Updates to main program overview docstring

--- PT_Plan.py
+++ PT_Plan.py
@@ -2,19 +2,11 @@ import openai
 
 '''
 Utilizing the OpenAI API, this program creates a baseline physical therapy
-plan for users. This is done by asking the prospective physical therapy patient
-to enter the number of weeks they plan to participate in physical therapy, the primary
-area in the body where they are experiencing pain, their current physical limitations,
-and their physical therapy goals. Because time is limited during a patient's first PT appointment,
+plan for users. Because time is limited during a patient's first PT appointment,
 the baseline plan is meant to complement the physical examination performed on the patient
 by their therapist and serve as a secondary source of data when creating the patient's final
 PT plan. Having more information regarding patient pain, limitations, and goals may help
 improve patient outcomes and ultimately help them achieve their physical therapy goals.
-
-In addition to creating the baseline plan, patients can also ask the AI Assistant to save their
-physical therapy plan to a file for them to take to their initial appointment, and they
-can also ask the AI Assistant to provide directions on one more of the exercises listed
-in their plan (e.g., how to perform 'toe raises').
 '''
 
 
